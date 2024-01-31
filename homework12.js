68064435870ff4aa61136016d95da4226ea70023
Dariya Kirdohlo <101713031+Dariashaki@users.noreply.github.com>
2024-01-31 20:25:07 +0200

    Description changes for  homework12.js

--- homework12.js
+++ homework12.js
@@ -3,7 +3,7 @@ var arr = [null, 'abc', '', ' ', 0, 13]; // наш рандомний масив
 function myBlend(arr) {
 // цикл який проходить по нашому масиву від індексу 0 до кінця
     for (var i = 0; i < arr.length; i++) {
-// генеруємо випадковий індекс - Math.random() генерує чісло від 0 до 1, Math.floor() округлює до цілого
+// генеруємо випадковий індекс: Math.random() генерує число від 0 до 1, Math.floor() округлює до цілого
 // множення на arr.length дає нам випадковий індекс у межах довжини нашого масиву
         var randomIndex = Math.floor(Math.random() * arr.length);
 // зберігаємо значення поточного елементу arr[i] у змінний temp
@@ -12,7 +12,7 @@ function myBlend(arr) {
         arr[i] = arr[randomIndex];
         arr[randomIndex] = temp;
     }
-    return arr; // кинець нашого циклу -> повертаємо наш перемишаній масив
+    return arr; // кінець нашого циклу -> повертаємо наш перемішаний масив
 }
 
 console.log(myBlend(arr)); // викликаємо нашу функцію
